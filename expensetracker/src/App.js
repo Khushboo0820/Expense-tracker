26290ddd72fc0f97d94328ae813017f08dfafa66
unknown <khushboo.parate74@gmail.com>
2024-10-24 23:09:52 +0530

    test commit

---
+++ expensetracker/src/App.js
@@ -0,0 +1,14 @@
+import Homepage from "./Homepage";
+import { SnackbarProvider } from "notistack";
+
+function App() {
+  return (
+    <SnackbarProvider>
+      <div>
+        <Homepage />
+      </div>
+    </SnackbarProvider>
+  );
+}
+
+export default App;
